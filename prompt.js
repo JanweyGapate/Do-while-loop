aa235e2d3b483bdc3e3e4a17913c8b1cb1356719
JanweyGapate <janweygapate@gmail.com>
2024-10-03 23:28:31 +0800

    Create prompt.js

---
+++ prompt.js
@@ -0,0 +1,11 @@
+let correctPassword = "Nancy daisy";
+
+let password ="Nancy daisy";
+
+
+do {if (password === correctPassword) 
+    {console.log("Access granted.");} 
+    else 
+    {console.log("Incorrect, try again.");
+    password ="estabillo19";}} 
+    while (password !== correctPassword);
